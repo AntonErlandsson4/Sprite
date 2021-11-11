e980eec3c6d3ecc3d5c73f60efaba8858270f530
AntonErlandsson4 <70326194+AntonErlandsson4@users.noreply.github.com>
2021-11-11 15:21:55 +0100

    Add files via upload

---
+++ Sprite.cs
@@ -0,0 +1,9 @@
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+class Sprite {
+    public Texture2D Texture {set; get;}
+    public Rectangle Rect {set; get;}
+}
